96dc5bd381e40522b142d29c3a4a13597c2b4b68
NRC07 <99951211+NRC07@users.noreply.github.com>
2022-03-25 08:29:08 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let Contrasenya = 0
 let Numero = 0
 let Lletra = 0
+let simbolo = 0
 input.onButtonPressed(Button.A, function () {
     basic.showString("Pass =")
     for (let index = 0; index < 8; index++) {
@@ -8,19 +9,37 @@ input.onButtonPressed(Button.A, function () {
         if (Contrasenya == 1) {
             Numero = randint(0, 9)
             basic.showString("" + (Numero))
+            basic.pause(2000)
         } else if (Contrasenya == 2) {
             Lletra = randint(5, 8)
             if (Lletra == 5) {
                 basic.showString("A")
+                basic.pause(2000)
             } else if (Lletra == 6) {
                 basic.showString("Z")
+                basic.pause(2000)
             } else if (Lletra == 7) {
                 basic.showString("N")
+                basic.pause(2000)
             } else if (Lletra == 8) {
                 basic.showString("P")
+                basic.pause(2000)
             }
-        } else if (false) {
-        	
+        } else if (Contrasenya == 3) {
+            simbolo = randint(1, 4)
+            if (simbolo == 1) {
+                basic.showString("*")
+                basic.pause(2000)
+            } else if (simbolo == 2) {
+                basic.showString("}")
+                basic.pause(2000)
+            } else if (simbolo == 3) {
+                basic.showString("#")
+                basic.pause(2000)
+            } else if (simbolo == 4) {
+                basic.showString("&")
+                basic.pause(2000)
+            }
         }
     }
 })
